e0808e761119065e6ddad5d100291997ce8e57f1
Robin Nedergård <60611967+robinnedergard@users.noreply.github.com>
2025-11-19 10:38:41 +0100

    fix: add version bumping (#4)

---
+++ release.config.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+        changelogTitle: '# Changelog',
+      },
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version}',
+      },
+    ],
+  ],
+}
