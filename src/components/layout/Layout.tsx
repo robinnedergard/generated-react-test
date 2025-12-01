4b5f91d7b764d8e5bfa7f16a34f7c63298384600
Robin Nedergård <need_87@hotmail.com>
2025-12-01 10:14:39 +0100

    fix: restore main page gap

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -15,7 +15,7 @@ export function Layout({ children, backLink }: LayoutProps) {
     <div className="max-w-[1200px] mx-auto px-5 lg:px-12 py-12 pb-16 flex flex-col gap-12">
       <Header />
       <CartSidebar />
-      <div className="px-8 py-16">
+      <div className="px-8 py-16 flex flex-col gap-12">
         {backLink && <BackLink to={backLink.to} label={backLink.label} />}
         {children}
       </div>
